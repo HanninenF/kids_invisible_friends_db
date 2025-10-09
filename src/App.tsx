2f4d779de2e46490f5deb59a4eafaec5a38810ba
Mustafa <raygal99@hotmail.com>
2025-10-09 12:21:07 +0200

    skapat card-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { type JSX } from 'react';
+import React, { useState } from "react";
+import "./App.scss";
+import CardForm from "./container/CardForm";
 
-import './App.scss';
+const App: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
 
-export default function App(): JSX.Element {
-  return <>
-    Hello Musse</>;
-}
+  return (
+    <div className="app-container">
+      <div className="sidebar">
+        <button className="btn">Lägg till barn–knapp</button>
+        <button className="btn">Lägg till osynlig vän–knapp</button>
+
+        <div className="CardForm-container">
+          <CardForm />
+        </div>
+
+        <button className="btn add">Add Barn</button>
+      </div>
+
+      <div className="card-container">
+        CARD CONTAINER
+      </div>
+    </div>
+  );
+};
+
+export default App;
